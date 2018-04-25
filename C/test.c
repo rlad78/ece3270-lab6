c7cdc15573c7a76ec344bb9f16f937db83acaa97
Richard Carter <goobysc@gmail.com>
2018-04-25 17:35:20 -0400

    ppm_copy test

--- C/test.c
+++ C/test.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-	ppmimg *in_img;
+	ppmimg *in_img, *out_img;
 
 	if (argc < 2)
 	{
@@ -12,12 +12,14 @@ int main(int argc, char const *argv[])
 	}
 
 	in_img = ppm_read(argv[1]);
-	ppm_write(in_img, argv[2]);
+	out_img = ppm_copy(in_img);
+	ppm_write(out_img, argv[2]);
 
 	printf("width = %u height = %u colormax = %u\nfirst pixel = %c\n", 
 		in_img->width, in_img->height, in_img->colormax, in_img->data[0][0][0]);
 
 	ppm_free(in_img);
+	ppm_free(out_img);
 
 	return 0;
 }
