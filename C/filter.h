f3503122f441b8053cfb151fce6dee01ffc37d75
Richard Carter <goobysc@gmail.com>
2018-04-25 19:43:47 -0400

    fuck pointers

--- C/filter.h
+++ C/filter.h
@@ -1,88 +1,79 @@
 #ifndef FILTER_H_
 #define FILTER_H_ 
 
-const char IDENTITY_ARR[5][5] =
+const char MASK_ARR[8][5][5] = {
+//IDENTITY_ARR
 	{
 		{0,0,0,0,0},
 		{0,0,0,0,0},
 		{0,0,1,0,0},
 		{0,0,0,0,0},
 		{0,0,0,0,0},
-	};
+	},
 
-const char EDGE1_ARR[5][5] =
+//EDGE1_ARR
 	{
 		{0,0,0,0,0},
 		{0,1,0,-1,0},
 		{0,0,0,0,0},
 		{0,-1,0,1,0},
 		{0,0,0,0,0},
-	};
+	},
 
-const char EDGE2_ARR[5][5] =
+//EDGE2_ARR
 	{
 		{0,0,0,0,0},
 		{0,0,1,0,0},
 		{0,1,-4,1,0},
 		{0,0,1,0,0},
 		{0,0,0,0,0},
-	};
+	},
 
-const char EDGE3_ARR[5][5] =
+//EDGE3_ARR
 	{
 		{0,0,0,0,0},
 		{0,-1,-1,-1,0},
 		{0,-1,8,-1,0},
 		{0,-1,-1,-1,0},
 		{0,0,0,0,0},
-	};
+	},
 
 
-const char SHARPEN_ARR[5][5] =
+//SHARPEN_ARR
 	{
 		{0,0,0,0,0},
 		{0,0,-1,0,0},
 		{0,-1,5,-1,0},
 		{0,0,-1,0,0},
 		{0,0,0,0,0},
-	};
+	},
 
-const char BOX_ARR[5][5] =
+//BOX_ARR
 	{
 		{0,0,0,0,0},
 		{0,1,1,1,0},
 		{0,1,1,1,0},
 		{0,1,1,1,0},
 		{0,0,0,0,0},
-	};
+	},
 
-const char GAUSS1_ARR[5][5] =
+//GAUSS1_ARR
 	{
 		{0,0,0,0,0},
 		{0,1,2,1,0},
 		{0,2,4,1,0},
 		{0,1,2,1,0},
 		{0,0,0,0,0},
-	};
+	},
 
-const char GAUSS2_ARR[5][5] =
+//GAUSS2_ARR
 	{
 		{1,4,6,4,1},
 		{4,16,24,16,4},
 		{6,24,36,24,6},
 		{4,16,24,16,4},
 		{1,4,6,4,1},
-	};
-
-const char ***MASK_ARR[8] = {
-	IDENTITY_ARR,
-	EDGE1_ARR,
-	EDGE2_ARR,
-	EDGE3_ARR,
-	SHARPEN_ARR,
-	BOX_ARR,
-	GAUSS1_ARR,
-	GAUSS2_ARR
+	}
 };
 
 const float MOD_ARR[8] = {
