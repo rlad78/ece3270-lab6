9e826f1b2bebf7615c31d42d0309a37eb464b38e
Richard Carter <goobysc@gmail.com>
2018-04-26 11:14:54 -0400

    gotta put enum back in .h apparently

--- C/filter.h
+++ C/filter.h
@@ -3,6 +3,17 @@
 
 extern const char MASK_ARR[8][5][5];
 extern const float MOD_ARR[8];
-extern enum filter;
+
+enum filter
+{
+	IDENTITY,
+	EDGE1,
+	EDGE2,
+	EDGE3,
+	SHARPEN,
+	BOX,
+	GAUSS1,
+	GAUSS2,
+};
 
 #endif
